f4db17829afaa6fa837214ece889531ed8d8b0fd
danil <daniil.muhametgaleev@yandex.ru>
2022-07-15 11:20:07 +0300

    fixed urls

--- p_library/views.py
+++ p_library/views.py
@@ -1,3 +1,27 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
+from p_library.models import Book, Author
+
+
+def books_list(request):
+    books = Book.objects.all()
+    for book in books:
+        return HttpResponse(book.title)
+
+
+def Author_list(request):
+    authorss=''
+    authors = Author.objects.all()
+    # return HttpResponse(authors)
+    for author in authors:
+        authorss=authorss+author.full_name+" "
+    return HttpResponse(authorss)
+
+def index(request):
+    template = loader.get_template('index.html')
+    books_count = Book.objects.all().count()
+    biblio_data = {"title": "мою библиотеку", "books_count": books_count}
+    return HttpResponse(template.render(biblio_data))
